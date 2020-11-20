999ef3780cee66bb73f3b7b4098989c8b7cf8c63
impozible1 <74678692+impozible1@users.noreply.github.com>
2020-11-20 13:52:40 -0600

    Update MainJavascript.js

--- MainJavascript.js
+++ MainJavascript.js
@@ -1,6 +1,4 @@
 //So this is the main JS file with your typical variables and other stuff like that.
 
- var example = {hellos: 121};
- object.keys(example); 
-if hellos = < 120 {window.confirm("sometext")
-};
+ var example = 120;
+ if example = 120 {alert("Hello World!")} else {alert("B-But thats impossible...")};
