12ebca8cca52796ee0daf04a43af772615c5f3ad
impozible1 <74678692+impozible1@users.noreply.github.com>
2020-11-20 10:26:52 -0600

    Update and rename JavascriptTestStuff.js to MainJavascript.js

--- JavascriptTestStuff.js
+++
@@ -1,4 +0,0 @@
-//None of this stuff is important its just a test for the Nodejs workflow because ive never used it before
-
- var example = {hellos: 121};
- object.keys(example);
---
+++ MainJavascript.js
@@ -0,0 +1,6 @@
+//So this is the main JS file with your typical variables and other stuff like that.
+
+ var example = {hellos: 121};
+ object.keys(example); 
+if hellos = < 120 {window.confirm("sometext")
+};
