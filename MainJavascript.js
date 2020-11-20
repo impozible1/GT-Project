3a55fa6873db4bd554e8d9ab191acf29aa0b90ba
impozible1 <74678692+impozible1@users.noreply.github.com>
2020-11-20 14:48:56 -0600

    Update MainJavascript.js

--- MainJavascript.js
+++ MainJavascript.js
@@ -1,4 +1,1 @@
-//So this is the main JS file with your typical variables and other stuff like that.
-
- var example = 120;
- if example = 120 {alert("Hello World!")} else {alert("B-But thats impossible...")};
+//So this is the main JS file where you will add JavaScript.
