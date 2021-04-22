321c25d557ab15f358faf3a4d6fc8bd0c622a376
impozible1 <74678692+impozible1@users.noreply.github.com>
2021-04-21 20:34:14 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,8 @@
 var character = document.getElementById("character");
+// Specifies the conditions that will start the jump function.
 document.addEventListener("click", jump);
 document.addEventListener("keydown", jump);
+
 function jump(){
   if(character.classList == "animate"){return;}
   character.classList.add("animate");
@@ -13,6 +15,7 @@ function removeJump(){
 
 
 var enemy = document.getElementById("block");
+// Checks if block has touched the player object.
 function checkDead(){
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
